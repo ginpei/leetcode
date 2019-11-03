9be0432d2014b07a586119f83475e88cb5e9bd55
ginpei <ginpei@gmail.com>
2019-11-02 17:04:00 -0700

    62. Unique Paths

---
+++ problems/0062-unique-paths/index.js
@@ -0,0 +1,45 @@
+/**
+ * 62. Unique Paths
+ *
+ * A robot is located at the top-left corner of a *m* x *n* grid (marked
+ * 'Start' in the diagram below).
+ *
+ * The robot can only move either down or right at any point in time. The
+ * robot is trying to reach the bottom-right corner of the grid (marked
+ * 'Finish' in the diagram below).
+ *
+ * How many possible unique paths are there?
+ *
+ * Note: *m* and *n* will be at most 100.
+ *
+ * @see https://leetcode.com/problems/unique-paths/
+ */
+
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+function uniquePaths(m, n) {
+  /** @type {number[]} */
+  let lastLine = [];
+  for (let i = 0; i < m; i += 1) {
+    lastLine.push(1);
+  }
+
+  for (let y = 1; y < n; y += 1) {
+    const line = [1];
+    for (let x = 1; x < m; x += 1) {
+      const above = lastLine[x];
+      const left = line[x - 1];
+      const count = above + left;
+      line.push(count);
+    }
+
+    lastLine = line;
+  }
+
+  return lastLine[m - 1];
+}
+
+module.exports.uniquePaths = uniquePaths;
---
+++ problems/0062-unique-paths/index.test.js
@@ -0,0 +1,23 @@
+const { uniquePaths } = require('./index');
+
+describe('0062 Unique Paths', () => {
+  it('example 1', () => {
+    expect(uniquePaths(3, 2)).toBe(3);
+  });
+
+  it('example 2', () => {
+    expect(uniquePaths(7, 3)).toBe(28);
+  });
+
+  it('horizontal line', () => {
+    expect(uniquePaths(1, 3)).toBe(1);
+  });
+
+  it('minimum square', () => {
+    expect(uniquePaths(2, 2)).toBe(2);
+  });
+
+  it('large matrix (exceeded)', () => {
+    expect(uniquePaths(51, 9)).toBe(1916797311);
+  });
+});
