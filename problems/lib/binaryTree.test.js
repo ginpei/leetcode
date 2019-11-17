1ece5c68a350b2e41ed3ebf2a30a3455cd13c665
ginpei <ginpei@gmail.com>
2019-11-16 21:57:43 -0800

    improve typing

--- problems/lib/binaryTree.test.js
+++ problems/lib/binaryTree.test.js
@@ -1,5 +1,3 @@
-// @ts-nocheck
-
 const lib = require('./binaryTree');
 
 describe('lib/binaryTree', () => {
@@ -15,16 +13,26 @@ describe('lib/binaryTree', () => {
       });
 
       it('sets the root', () => {
-        expect(root).not.toBeNull();
+        if (!root) {
+          throw new Error('root must not be null');
+        }
         expect(root.val).toBe(1);
       });
 
       it('sets the children of the root', () => {
+        if (!root || !root.left || !root.right) {
+          throw new Error('Target nodes must not be null');
+        }
+
         expect(root.left.val).toBe(2);
         expect(root.right.val).toBe(3);
       });
 
       it('leaves null if no inputs', () => {
+        if (!root || !root.left || !root.right) {
+          throw new Error('Target nodes must not be null');
+        }
+
         expect(root.left.left).toBeNull();
         expect(root.left.right).toBeNull();
         expect(root.right.left).toBeNull();
@@ -32,8 +40,48 @@ describe('lib/binaryTree', () => {
       });
     });
 
-    describe('for leaf nodes', () => {
-      const input = [1, 2, 3, null, null, 6, 7, 8];
+    describe('for ones having left child', () => {
+      const input = [1, 2];
+
+      /** @type {TreeNode<number> | null} */
+      let root;
+
+      beforeEach(() => {
+        root = lib.buildBinaryTree([...input]);
+      });
+
+      it('sets the children of the root', () => {
+        if (!root || !root.left) {
+          throw new Error('Target nodes must not be null');
+        }
+
+        expect(root.left.val).toBe(2);
+        expect(root.right).toBeNull();
+      });
+    });
+
+    describe('for ones having right child', () => {
+      const input = [1, null, 3];
+
+      /** @type {TreeNode<number> | null} */
+      let root;
+
+      beforeEach(() => {
+        root = lib.buildBinaryTree([...input]);
+      });
+
+      it('sets the children of the root', () => {
+        if (!root || !root.right) {
+          throw new Error('Target nodes must not be null');
+        }
+
+        expect(root.left).toBeNull();
+        expect(root.right.val).toBe(3);
+      });
+    });
+
+    describe('for leaf nodes in between', () => {
+      const input = [1, 2, 3, null, null, 6, 7];
 
       /** @type {TreeNode<number> | null} */
       let root;
@@ -43,8 +91,23 @@ describe('lib/binaryTree', () => {
       });
 
       it('leave null if inputs are null', () => {
+        if (!root || !root.left) {
+          throw new Error('Target nodes must not be null');
+        }
+
+        expect(root.left.val).toBe(2);
         expect(root.left.left).toBeNull();
         expect(root.left.right).toBeNull();
+      });
+
+      it('sets values after null slots', () => {
+        if (!root || !root.right || !root.right.left || !root.right.right) {
+          throw new Error('Target nodes must not be null');
+        }
+
+        expect(root.right.val).toBe(3);
+        expect(root.right.left.val).toBe(6);
+        expect(root.right.right.val).toBe(7);
       });
     });
   });
