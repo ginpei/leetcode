60af3c96145327fed6640dc311c5e55802320a77
ginpei <ginpei@gmail.com>
2019-11-16 16:15:34 -0800

    257. Binary Tree Paths

---
+++ problems/0257-binary-tree-paths/index.js
@@ -0,0 +1,31 @@
+/**
+ * 257. Binary Tree Paths
+ *
+ * Given a binary tree, return all root-to-leaf paths.
+ *
+ * @see https://leetcode.com/problems/binary-tree-paths/
+ */
+
+/**
+ * @param {import("../../types/types").TreeNode<number> | null} root
+ * @param {string} stack
+ * @return {string[]}
+ */
+function binaryTreePaths(root, stack = '') {
+  if (!root) {
+    return [];
+  }
+
+  const curStack = stack ? `${stack}->${root.val}` : `${root.val}`;
+
+  if (!root.left && !root.right) {
+    return [curStack];
+  }
+
+  return [
+    ...binaryTreePaths(root.left, curStack),
+    ...binaryTreePaths(root.right, curStack),
+  ].filter((v) => v);
+}
+
+module.exports.binaryTreePaths = binaryTreePaths;
