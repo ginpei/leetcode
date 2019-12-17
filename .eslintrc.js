f5d0c47146c8b95a5fe3fd20f52ac1319981a7cc
ginpei <ginpei@gmail.com>
2019-12-16 20:10:20 -0800

    add TS support to ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,18 @@
 module.exports = {
   "extends": [
     "eslint-config-airbnb-base",
+    "plugin:@typescript-eslint/recommended",
   ],
   "env": {
-    "browser": true,
     "es6": true,
     "jest": true,
   },
+  "parser": "@typescript-eslint/parser",
 	"rules": {
+    "@typescript-eslint/ban-ts-ignore": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-use-before-define": "off",
+    "@typescript-eslint/no-var-requires": "off",
     "arrow-parens": ["error", "always"],
     "class-methods-use-this": "off",
     "import/prefer-default-export": "off",
