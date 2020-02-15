fb1fac980e8c6958634cacd7bb872b6558462c03
ginpei <ginpei@gmail.com>
2020-02-15 15:32:23 -0800

    209. Minimum Size Subarray Sum

---
+++ problems/0209-minimum-size-subarray-sum/index.js
@@ -0,0 +1,32 @@
+/**
+ * 209. Minimum Size Subarray Sum
+ * @see https://leetcode.com/problems/minimum-size-subarray-sum/
+ */
+
+/**
+ * @param {number} s
+ * @param {number[]} nums
+ * @return {number}
+ */
+function minSubArrayLen(s, nums) {
+  let min = 0;
+  let iStart = 0;
+  let sum = 0;
+
+  for (let iEnd = 0; iEnd < nums.length; iEnd++) {
+    sum += nums[iEnd];
+
+    for (iStart; sum >= s && iStart < nums.length; iStart++) {
+      const length = (iEnd + 1) - iStart;
+      if (sum >= s && (min === 0 || length < min)) {
+        min = length;
+      }
+
+      sum -= nums[iStart];
+    }
+  }
+
+  return min;
+}
+
+module.exports.minSubArrayLen = minSubArrayLen;
---
+++ problems/0209-minimum-size-subarray-sum/index.test.js
@@ -0,0 +1,23 @@
+const { minSubArrayLen } = require('./index');
+
+describe('209. Minimum Size Subarray Sum', () => {
+  it('example', () => {
+    expect(minSubArrayLen(7, [2, 3, 1, 2, 4, 3])).toBe(2);
+  });
+
+  it('WA: empty array', () => {
+    expect(minSubArrayLen(100, [])).toBe(0);
+  });
+
+  it('WA: block placed at the end', () => {
+    expect(minSubArrayLen(4, [1, 4, 4])).toBe(1);
+  });
+
+  it('block placed at the end', () => {
+    expect(minSubArrayLen(4, [1, 4, 4, 1])).toBe(1);
+  });
+
+  it('WA: no match', () => {
+    expect(minSubArrayLen(3, [1, 1])).toBe(0);
+  });
+});
