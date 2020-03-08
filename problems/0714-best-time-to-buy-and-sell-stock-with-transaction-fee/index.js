47ab481c76c96002fbe0319e4a33b356f3e0e2e1
ginpei <ginpei@gmail.com>
2020-03-07 21:55:05 -0800

    714. Best Time to Buy and Sell Stock with Transaction Fee

---
+++ problems/0714-best-time-to-buy-and-sell-stock-with-transaction-fee/index.js
@@ -0,0 +1,43 @@
+/**
+ * 714. Best Time to Buy and Sell Stock with Transaction Fee
+ *
+ * @see https://leetcode.com/problems/0714-best-time-to-buy-and-sell-stock-with-transaction-fee
+ */
+
+/**
+ * @param {number[]} prices
+ * @param {number} fee
+ * @return {number}
+ */
+function maxProfit(prices, fee) {
+  let profit = 0;
+  let min = prices[0];
+  let max = min;
+
+  // console.log('# prices, fee', prices, fee);
+  for (let i = 1; i < prices.length; i++) {
+    const cur = prices[i];
+    // console.log(`# [${i}:${cur}] --------`);
+    if (cur > max) {
+      max = cur;
+    } else if (cur < min || cur < (max - fee)) {
+      // console.log(`# [${i}:${cur}] dealt`, Math.max(0, (max - fee) - min));
+      profit += Math.max(0, (max - fee) - min);
+      min = cur;
+      max = cur;
+    }
+
+    const last = i + 1 === prices.length;
+    if (last) {
+      const curProfit = Math.max(0, max - min - fee);
+      if (curProfit > 0) {
+        profit += curProfit;
+      }
+    }
+    // console.log(`# [${i}:${cur}] ->`, [min, max], profit);
+  }
+
+  return profit;
+}
+
+module.exports.maxProfit = maxProfit;
---
+++ problems/0714-best-time-to-buy-and-sell-stock-with-transaction-fee/index.test.js
@@ -0,0 +1,39 @@
+const { maxProfit } = require('./index');
+
+describe('714. Best Time to Buy and Sell Stock with Transaction Fee', () => {
+  it('example 1', () => {
+    expect(maxProfit([1, 3, 2, 8, 4, 9], 2)).toBe(8);
+  });
+
+  it('wa 1', () => {
+    expect(maxProfit([1, 4, 6, 2, 8, 3, 10, 14], 3)).toBe(13);
+  });
+
+  it('wa 2: not less than fee', () => {
+    expect(maxProfit([1, 3, 7, 5, 10, 3], 3)).toBe(6);
+  });
+
+  it('wa 3: less than fee', () => {
+    expect(maxProfit([4, 5, 2, 4, 3, 3, 1, 2, 5, 4], 1)).toBe(4);
+  });
+
+  it('wa 4: second is less than the first', () => {
+    expect(maxProfit([2, 1, 4, 4, 2, 3, 2, 5, 1, 2], 1)).toBe(4);
+  });
+
+  it('wa 5', () => {
+    expect(maxProfit([2, 2, 1, 1, 5, 5, 3, 1, 5, 4], 2)).toBe(4);
+  });
+
+  it('goes through if gap is less than fee', () => {
+    expect(maxProfit([1, 5, 4, 9], 2)).toBe(6);
+  });
+
+  it('goes through if gap is equal to fee', () => {
+    expect(maxProfit([1, 5, 3, 9], 2)).toBe(6);
+  });
+
+  it('sells once if gap is more than fee', () => {
+    expect(maxProfit([1, 5, 2, 9], 2)).toBe(7);
+  });
+});
