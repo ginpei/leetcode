8b44c794febf0c95cd31111cdbe6bd80128c4922
ginpei <ginpei@gmail.com>
2020-04-25 13:27:33 -0700

    update 55. Jump Game

--- problems/0055-jump-game/index.js
+++ problems/0055-jump-game/index.js
@@ -8,30 +8,11 @@
  * @return {boolean}
  */
 function canJump(nums) {
-  let p = 0;
-  // let count = 0;
-  while (p < nums.length - 1) {
-    // if (count++ > 1000) { throw new Error('infinite'); }
-
-    // console.log(`# nums[${p}]=${nums[p]}`);
-    if (nums[p] === 0) {
-      for (let q = p - 1; q >= 0; q--) {
-        // console.log('#', nums, `nums[${q}]=${nums[q]}`, p - q);
-        if (nums[q] > p - q) {
-          p = q;
-          break;
-        }
-      }
-
-      if (nums[p] === 0) {
-        return false;
-      }
-    }
-
-    p += nums[p];
+  let furthest = 0;
+  for (let i = 0; i < nums.length && i <= furthest; i++) {
+    furthest = Math.max(furthest, i + nums[i]);
   }
-
-  return true;
+  return furthest >= nums.length - 1;
 }
 
 module.exports.canJump = canJump;
