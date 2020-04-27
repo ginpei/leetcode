ec9e375059acfb9939815fe331f91ecce298de1b
ginpei <ginpei@gmail.com>
2020-04-27 00:53:13 -0700

    221. Maximal Square

---
+++ problems/0221-maximal-square/index.js
@@ -0,0 +1,30 @@
+/**
+ * 221. Maximal Square
+ * @see https://leetcode.com/problems/maximal-square/
+ */
+
+/**
+ * @param {string[][]} matrix
+ * @return {number}
+ */
+function maximalSquare(matrix) {
+  /** @type {number[][]} */
+  const history = Array.from({ length: matrix.length }, () => []);
+  let max = 0;
+  matrix.forEach((line, row) => line.forEach((c, col) => {
+    if (c === '1') {
+      history[row][col] = 1 + Math.min(
+        (history[row - 1] || [])[col - 1] || 0, // left above
+        (history[row - 1] || [])[col] || 0, // above
+        (history[row] || [])[col - 1] || 0, // left
+      );
+      max = Math.max(max, history[row][col]);
+    } else {
+      history[row][col] = 0;
+    }
+  }));
+  // console.log('# \n', history);
+  return max ** 2;
+}
+
+module.exports.maximalSquare = maximalSquare;
---
+++ problems/0221-maximal-square/index.test.js
@@ -0,0 +1,36 @@
+const { maximalSquare } = require('./index');
+
+describe('221. Maximal Square', () => {
+  it('example', () => {
+    expect(maximalSquare(chopIntoInput(`
+      1 0 1 0 0
+      1 0 1 1 1
+      1 1 1 1 1
+      1 0 0 1 0
+    `))).toBe(4);
+  });
+
+  it('lack of corner', () => {
+    expect(maximalSquare(chopIntoInput(`
+      1 1 0
+      1 1 1
+      0 1 1
+    `))).toBe(4);
+  });
+
+  it('empty', () => {
+    expect(maximalSquare([])).toBe(0);
+  });
+
+  it('minimum', () => {
+    expect(maximalSquare([['1']])).toBe(1);
+  });
+
+  /**
+   * @param {string} s
+   * @returns {string[][]}
+   */
+  function chopIntoInput(s) {
+    return s.trim().split('\n').map((v) => v.trim().split(' '));
+  }
+});
