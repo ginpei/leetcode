c7a4e571ef761b7c96e83fc919aa431227352dc8
ginpei <ginpei@gmail.com>
2020-05-11 22:38:53 -0700

    update 733. Flood Fill

--- problems/0733-flood-fill/index.js
+++ problems/0733-flood-fill/index.js
@@ -1,5 +1,3 @@
-const grid = require('../lib/grid');
-
 /**
  * 733. Flood Fill
  *
@@ -29,7 +27,38 @@ const grid = require('../lib/grid');
  * @return {number[][]}
  */
 function floodFill(image, sr, sc, newColor) {
-  return grid.floodFill(image, sr, sc, newColor);
+  const oldColor = image[sr][sc];
+
+  if (newColor !== oldColor) {
+    advance(image, sr, sc, newColor, oldColor);
+  }
+
+  return image;
+}
+
+/**
+ * @param {number[][]} image Gets destructed internally
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} newColor
+ * @param {number} oldColor
+ */
+function advance(image, sr, sc, newColor, oldColor) {
+  if (sr < 0 || image.length <= sr || sc < 0 || image[0].length <= sc) {
+    return;
+  }
+
+  if (image[sr][sc] !== oldColor) {
+    return;
+  }
+
+  // eslint-disable-next-line no-param-reassign
+  image[sr][sc] = newColor;
+
+  advance(image, sr - 1, sc, newColor, oldColor);
+  advance(image, sr + 1, sc, newColor, oldColor);
+  advance(image, sr, sc - 1, newColor, oldColor);
+  advance(image, sr, sc + 1, newColor, oldColor);
 }
 
 module.exports.floodFill = floodFill;
