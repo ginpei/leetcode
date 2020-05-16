b27cc57e898c8524e39ce34ab697ea6866b21012
ginpei <ginpei@gmail.com>
2020-05-16 01:51:48 -0700

    918. Maximum Sum Circular Subarray

---
+++ problems/0918-maximum-sum-circular-subarray/index.js
@@ -0,0 +1,35 @@
+/**
+ * 918. Maximum Sum Circular Subarray
+ * @see https://leetcode.com/problems/maximum-sum-circular-subarray/
+ * @see https://leetcode.com/problems/maximum-sum-circular-subarray/discuss/633058/Java-or-C%2B%2B-or-Python3-or-With-detailed-explanation-or-O(N)-time-or-O(1)
+ */
+
+/**
+ * @param {number[]} A
+ * @return {number}
+ */
+function maxSubarraySumCircular(A) {
+  let sum = A[0];
+  let max = A[0];
+  let maxTotal = A[0];
+  let min = A[0];
+  let minTotal = A[0];
+
+  for (let i = 1; i < A.length; i++) {
+    const n = A[i];
+    max = Math.max(n, max + n);
+    maxTotal = Math.max(maxTotal, max);
+
+    min = Math.min(n, min + n);
+    minTotal = Math.min(minTotal, min);
+
+    sum += n;
+  }
+
+  const result = sum === minTotal
+    ? maxTotal
+    : Math.max(sum - minTotal, maxTotal);
+  return result;
+}
+
+module.exports.maxSubarraySumCircular = maxSubarraySumCircular;
---
+++ problems/0918-maximum-sum-circular-subarray/index.test.js
@@ -0,0 +1,23 @@
+const { maxSubarraySumCircular } = require('./index');
+
+describe('918. Maximum Sum Circular Subarray', () => {
+  it('example 1', () => {
+    expect(maxSubarraySumCircular([1, -2, 3, -2])).toBe(3);
+  });
+
+  it('example 2', () => {
+    expect(maxSubarraySumCircular([5, -3, 5])).toBe(10);
+  });
+
+  it('example 3', () => {
+    expect(maxSubarraySumCircular([3, -1, 2, -1])).toBe(4);
+  });
+
+  it('example 4', () => {
+    expect(maxSubarraySumCircular([3, -2, 2, -3])).toBe(3);
+  });
+
+  it('example 5', () => {
+    expect(maxSubarraySumCircular([-2, -3, -1])).toBe(-1);
+  });
+});
