3a430e280380aa5c0e78b68f628ccafa71b1dc73
ginpei <ginpei@gmail.com>
2020-05-22 01:22:21 -0700

    451. Sort Characters By Frequency

---
+++ problems/0451-sort-characters-by-frequency/index.js
@@ -0,0 +1,24 @@
+/**
+ * 451. Sort Characters By Frequency
+ * @see https://leetcode.com/problems/sort-characters-by-frequency/
+ */
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+function frequencySort(s) {
+  /** @type {Map<string, string>} */
+  const counts = new Map();
+  [...s].forEach((c) => {
+    const line = counts.get(c) || '';
+    counts.set(c, line + c);
+  });
+
+  return [...counts.entries()]
+    .sort(([, s1], [, s2]) => s2.length - s1.length)
+    .map(([, v]) => v)
+    .join('');
+}
+
+module.exports.frequencySort = frequencySort;
