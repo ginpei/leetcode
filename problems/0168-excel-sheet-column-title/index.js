5e85e07e511ddc4a8b307742b43f5377dadb8a18
ginpei <ginpei@gmail.com>
2020-09-04 01:21:39 -0700

    168. Excel Sheet Column Title

---
+++ problems/0168-excel-sheet-column-title/index.js
@@ -0,0 +1,22 @@
+/**
+ * 168. Excel Sheet Column Title
+ * @see https://leetcode.com/problems/excel-sheet-column-title/
+ */
+
+const offset = 'A'.charCodeAt(0) - 1;
+
+/**
+ * @param {number} n 1, 2, 3, ...
+ * @returns {string}
+ */
+function convertToTitle(n) {
+  const here = n % 26;
+  const letter = here === 0 ? 'Z' : String.fromCharCode(here + offset);
+
+  const rest = Math.floor((n - 1) / 26);
+  const uppers = n > 26 ? convertToTitle(rest) : '';
+
+  return `${uppers}${letter}`;
+}
+
+module.exports.convertToTitle = convertToTitle;
---
+++ problems/0168-excel-sheet-column-title/index.test.js
@@ -0,0 +1,31 @@
+const { convertToTitle } = require('.');
+
+describe('168. Excel Sheet Column Title', () => {
+  it('example 1', () => {
+    expect(convertToTitle(1)).toBe('A');
+  });
+
+  it('the last 1 digit', () => {
+    expect(convertToTitle(26)).toBe('Z');
+  });
+
+  it('the first 2 digits', () => {
+    expect(convertToTitle(27)).toBe('AA');
+  });
+
+  it('example 2', () => {
+    expect(convertToTitle(28)).toBe('AB');
+  });
+
+  it('example 3', () => {
+    expect(convertToTitle(701)).toBe('ZY');
+  });
+
+  it('the last 2 digits', () => {
+    expect(convertToTitle(702)).toBe('ZZ');
+  });
+
+  it('the first 3 digits', () => {
+    expect(convertToTitle(703)).toBe('AAA');
+  });
+});
